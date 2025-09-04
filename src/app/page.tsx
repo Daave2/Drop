b2f6353e2b3a7427d9b6256723163bfaca716503
Niki Cooke <DAVE3K@gmail.com>
2025-09-04 13:03:07 +0000

    AR not allowed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,50 +2,14 @@
 "use client";
 
 import MapView from "@/components/map-view";
-import { Suspense, useEffect, useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "@/components/ui/dialog";
-import Link from "next/link";
-import { trackEvent } from "@/lib/analytics";
+import { Suspense } from "react";
 
 export default function Home() {
-  const [unsupported, setUnsupported] = useState(false);
-
-  useEffect(() => {
-    const supported =
-      typeof window !== "undefined" &&
-      typeof navigator.mediaDevices?.getUserMedia === "function" &&
-      "DeviceOrientationEvent" in window &&
-      (navigator as any).xr;
-    if (!supported) {
-      setUnsupported(true);
-      trackEvent("ar_launch_failed", { reason: "unsupported_environment" });
-    }
-  }, []);
-
   return (
     <>
       <Suspense>
         <MapView />
       </Suspense>
-      <Dialog open={unsupported}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>AR Not Supported</DialogTitle>
-            <DialogDescription>
-              Your device or browser lacks camera or motion sensor support required for AR mode.
-            </DialogDescription>
-            <Link href="/" className="underline mt-4 block">
-              Return to Map
-            </Link>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
     </>
   );
 }
